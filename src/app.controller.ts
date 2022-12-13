9ea04c4a937346fbec73ffc3c38554d79b50acb7
Pedro Manoel <pedro.alves@ccc.ufcg.edu.br>
2022-12-13 13:15:23 -0300

    chore: desenvolvimento da primeira estrutura do projeto

---
+++ src/app.controller.ts
@@ -0,0 +1,28 @@
+import { CreateNotificationBody } from './create-notification-body';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { content, category, recipientId } = body;
+
+    return this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
+  }
+}
