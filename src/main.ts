175e03166eb1d312db25222f38e898aeac29501b
Pedro Manoel <pedro.alves@ccc.ufcg.edu.br>
2023-03-22 12:56:58 -0300

    mudando de express para fastify

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,19 @@ import { setSwagger } from '@infra/docs/swagger/setup/set-swagger';
 import { setPipes } from '@infra/http/setup/set-pipes';
 import { setKafka } from '@infra/messaging/kafka/setup/set-kafka';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 import { AppModule } from './app.module';
 import { GlobalConfig } from './config/global/global.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   const globalConfig = app.get(GlobalConfig);
 
   setSwagger(app);
